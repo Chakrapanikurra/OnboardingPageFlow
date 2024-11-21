f7b02b8e00ab94fe35a908edc724b1a0995c9f8a
Chakrapanikurra <chakra.kurra@gmail.com>
2024-11-21 16:56:31 -0700

    Initial Commit

---
+++ OnboardingPageFlow/OnboardingPageFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingPageFlowApp.swift
+//  OnboardingPageFlow
+//
+//  Created by Chakrapani Kurra on 2024-11-21.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingPageFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
